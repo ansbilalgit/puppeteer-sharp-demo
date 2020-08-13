3c64cccff3e06036bf238f6bfb789fccb92b5a9d
Ans bilal <ans.bilal@sixlogics.com>
2020-08-13 11:35:50 +0500

    initial commit

---
+++ puppeteer-demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puppeteer_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testPuppetAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Happy Hurray");
+            Console.ReadKey();
+        }
+
+        private static async Task testPuppetAsync()
+        {
+            string baseUrl = "https://www.pgatour.com/leaderboard.html";
+            Console.WriteLine("Loading Url " + baseUrl);
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            using (Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false
+            }))
+            {
+                Page page = await browser.NewPageAsync();
+
+                await page.SetRequestInterceptionAsync(true);
+                page.Request += OnPageRequest;
+                await page.GoToAsync(baseUrl);
+            }
+        }
+
+        private static async void OnPageRequest(object sender, RequestEventArgs requestEvent)
+        {
+            string requestUrl = requestEvent.Request?.Url;
+            if (requestUrl.Contains("leaderboard.json"))
+                Console.WriteLine(requestEvent.Request?.Url);
+            await requestEvent.Request.ContinueAsync();
+        }
+
+
+    }
+}
